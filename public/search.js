ef33c37f4c2fb755ed3897dbd399b096436d99de
Luis <a01722501@tec.mx>
2024-04-21 21:40:21 -0600

    Boton que tiene embedded de unity

---
+++ public/search.js
@@ -0,0 +1,7 @@
+document.getElementById("searchButton").addEventListener("click", function() {
+    // Define the URL to search
+    var url = "http://localhost:50051";
+
+    // Open the URL in a new tab/window
+    window.open(url, "_blank");
+});
