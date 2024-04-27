3bae3aec351858bef49a0938ee8b49ef6038f37d
JulenHopp <julenhoppenstedt@gmail.com>
2024-04-27 15:49:31 -0600

    Integrando buscaborrego a la pagina web

---
+++ public/scripts/games.js
@@ -0,0 +1,33 @@
+function openBuscaBorrego(dineroIncial, numeroMinas) {
+    // Preparar los datos a enviar en la solicitud POST
+    const postData = {
+        dinero: dineroIncial,
+        minas: numeroMinas
+    };
+
+    // Usar la API fetch para enviar la solicitud POST
+    fetch('/buscaBorrego/variables', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(postData)
+    })
+    .then(response => {
+        if (response.ok) {
+            // Si la solicitud POST fue exitosa, abrir el juego en una ventana emergente
+            const features = "width=1440,height=810,left=200,top=200,menubar=no,toolbar=no,location=no,status=no,resizable=yes";
+            window.open(`/buscaBorrego/index.html`, 'BuscaBorrego', features);
+        } else {
+            // Si hubo un problema con la solicitud POST, loguear el error
+            console.error('Failed to update game variables');
+            return response.text().then(text => { throw new Error(text); });
+        }
+    })
+    .catch(error => {
+        // Loguear cualquier error que ocurrió durante el fetch
+        console.error('Error during fetch:', error);
+    });
+}
+
+
